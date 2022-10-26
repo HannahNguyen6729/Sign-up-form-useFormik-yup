7a39f39b228fc54409ca7f673329b053145202b5
Hanh Nguyen <hanh.nguyen@Hanhs-MBP.elisakoti>
2022-10-26 21:09:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "50%", margin: "0 auto " }}>
+      <h1>Sign up </h1>
+      <SignupForm />
     </div>
   );
 }
